dc21af2c40595b309184e02e021b7faafcfd3939
haniehst <s_hanieh@live.concordia.ca>
2024-11-24 18:45:45 -0500

    Fixed dependencies

--- app/controllers/WebSocketController.java
+++ app/controllers/WebSocketController.java
@@ -2,19 +2,16 @@ package controllers;
 
 import akka.actor.ActorSystem;
 import akka.stream.Materializer;
+import akka.stream.OverflowStrategy;
+import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 import akka.stream.javadsl.Keep;
-import akka.stream.OverflowStrategy;
-import akka.stream.QueueOfferResult;
-import akka.stream.scaladsl.Flow;
+import akka.stream.javadsl.SourceQueueWithComplete;
 import play.mvc.Controller;
 import play.mvc.WebSocket;
-import play.libs.streams.ActorFlow;
 
 import javax.inject.Inject;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 
 public class WebSocketController extends Controller {
 
@@ -31,21 +28,18 @@ public class WebSocketController extends Controller {
         return WebSocket.Text.accept(request -> createFlow());
     }
 
-    private CompletionStage<Flow<String, String, ?>> createFlow() {
+    private Flow<String, String, SourceQueueWithComplete<String>> createFlow() {
         // Define a Source to emit outgoing messages
-        Source<String, akka.stream.javadsl.SourceQueueWithComplete<String>> source =
+        Source<String, SourceQueueWithComplete<String>> source =
                 Source.queue(10, OverflowStrategy.dropHead());
 
         // Define a Sink to handle incoming messages
-        Sink<String, CompletionStage<akka.Done>> sink = Sink.foreach(message -> {
+        Sink<String, ?> sink = Sink.foreach(message -> {
             System.out.println("Received: " + message);
-            // You can process the incoming message here
+            // Process the incoming message
         });
 
         // Combine Sink and Source into a Flow
-        Flow<String, String, akka.stream.javadsl.SourceQueueWithComplete<String>> flow =
-                Flow.fromSinkAndSourceMat(sink, source, Keep.right());
-
-        return CompletableFuture.completedFuture(flow);
+        return Flow.fromSinkAndSourceMat(sink, source, Keep.right());
     }
 }
--- build.sbt
+++ build.sbt
@@ -8,30 +8,14 @@ scalaVersion := "2.13.15"
 javacOptions ++= Seq("-source", "11", "-target", "11")
 
 libraryDependencies ++= Seq(
+  // Play dependencies
   guice,
-  "org.apache.pekko" %% "pekko-actor" % "1.0.1" // Match Pekko version
-)
-libraryDependencies += "org.json" % "json" % "20210307"
-libraryDependencies += "org.mockito" % "mockito-core" % "4.0.0" % Test
-libraryDependencies += "junit" % "junit" % "4.13.2" % Test
-libraryDependencies += "edu.stanford.nlp" % "stanford-corenlp" % "4.4.0"
-libraryDependencies += "edu.stanford.nlp" % "stanford-corenlp" % "4.4.0" classifier "models"
-libraryDependencies += "org.apache.commons" % "commons-text" % "1.12.0"
 
-// The newer Akka version you want to use.
-val akkaVersion = "2.6.20"
-
-// Akka dependencies used by Play
-libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor"                 % akkaVersion,
-  "com.typesafe.akka" %% "akka-actor-typed"           % akkaVersion,
-  "com.typesafe.akka" %% "akka-stream"                % akkaVersion,
-  "com.typesafe.akka" %% "akka-slf4j"                 % akkaVersion,
-  "com.typesafe.akka" %% "akka-serialization-jackson" % akkaVersion,
-  "com.typesafe.akka" %% "akka-stream-typed" % akkaVersion,
-
-  // Only if you are using Akka Testkit
-  "com.typesafe.akka" %% "akka-testkit" % akkaVersion
+  // Core libraries
+  "org.json" % "json" % "20210307",
+  "org.mockito" % "mockito-core" % "4.0.0" % Test,
+  "junit" % "junit" % "4.13.2" % Test,
+  "org.apache.commons" % "commons-text" % "1.12.0"
 )
 
 // Set Javadoc options to include private members
